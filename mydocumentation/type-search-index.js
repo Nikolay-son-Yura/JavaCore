51a6fc17918902237e7f94d4370a2e5526d74595
Колька <nikola_doma@mail.ru>
2024-05-11 19:06:17 +0700

    Initial commit

---
+++ mydocumentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.gb.viktorov","l":"Decorator"},{"p":"ru.gb.viktorov","l":"Main"},{"p":"ru.gb.viktorov","l":"OtherClass"}];updateSearchResults();
